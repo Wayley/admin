cc0e4539d61a8624f84c3898250c9630db8c2786
wayley <hb_wangzheng@163.com>
2019-12-11 09:56:50 +0800

    init the project

---
+++ src/components/Welcome.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="welcome">WELCOME</div>
+</template>
+
+<script>
+export default {
+  name: 'Welcome',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.welcome {
+  text-align: center;
+  font-size: 24px;
+}
+</style>
